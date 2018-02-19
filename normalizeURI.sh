c1043e53c548ff54884aaf3d133158b9bcd20806
ivan <byteofmydream@gmail.com>
2018-02-19 07:58:33 +0200

    code comment

--- normalizeURI.sh
+++ normalizeURI.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-#add closing slash symbol after actual and expected URI's
+#add closing slash symbol in URI if it's not present
 slash="/"
 uri=`cat`
 lastChar=${uri: -1}
-
 if [ "$lastChar" != "$slash" ]
 then
 uri=${uri}"/"
