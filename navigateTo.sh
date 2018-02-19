42b1c2fda4bb476dc684515dbb95da72cbf789a2
ivan <byteofmydream@gmail.com>
2018-02-19 06:35:27 +0200

    navigateTo command fix

--- navigateTo.sh
+++ navigateTo.sh
@@ -2,4 +2,5 @@
 url=${1}
 sessionId=`cat ./sessionId`
 url="http://127.0.0.1:4444/wd/hub/session/$sessionId/url"
-curl -s -X "POST" ${url} -H 'Pragma: no-cache' -H 'Origin: http://127.0.0.1:4444' -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json; charset=utf-8' --data-binary '{"desiredCapabilities":{"browserName":"chrome"}}' --compressed
+json={"url":"$url"}
+curl -s -X "POST" ${url} -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json; charset=utf-8'  --data "$json"
