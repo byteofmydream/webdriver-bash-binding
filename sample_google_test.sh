74e725cbe9e4e9f11ef00963a45db35307d5114e
ivan <byteofmydream@gmail.com>
2018-02-19 08:17:28 +0200

    no sleep is bad :(

--- sample_google_test.sh
+++ sample_google_test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #this is example of simple UI test
 ./wd.sh start
+sleep 3s
 ./wd.sh get "https://google.com"
+sleep 3s
 ./wd.sh assertLocation "https://google.com"
 ./wd.sh quit
