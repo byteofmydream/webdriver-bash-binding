d22469054a4d4de0af348510d5dc35aa6cbde7b1
ivan <byteofmydream@gmail.com>
2018-02-24 01:57:31 +0200

    mirroring (escaping???)

--- navigateTo.sh
+++ navigateTo.sh
@@ -3,5 +3,5 @@ set -x
 url=${1}
 sessionId=`cat ./sessionId`
 hubEP="http://127.0.0.1:4444/wd/hub/session/$sessionId/url"
-json={"url":"$url"}
+json="{\"url\":\"$url\"}"
 curl -s -X "POST" ${hubEP} -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json; charset=utf-8'  --data "$json"
