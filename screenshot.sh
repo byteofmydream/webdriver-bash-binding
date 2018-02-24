76c92680ae76c2ee184b936e735c5ea0a4688119
ivan <byteofmydream@gmail.com>
2018-02-24 02:08:16 +0200

    screenshoter to file

--- screenshot.sh
+++ screenshot.sh
@@ -2,4 +2,5 @@
 set -x
 sessionId=`cat ./sessionId`
 url="http://127.0.0.1:4444/wd/hub/session/$sessionId/screenshot"
-curl -s ${url} -H 'Pragma: no-cache' -H 'Origin: http://127.0.0.1:4444' -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json; charset=utf-8'
+resp=`curl -s ${url} -H 'Pragma: no-cache' -H 'Origin: http://127.0.0.1:4444' -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json; charset=utf-8'`
+echo ${resp} | ./getFromJSON.sh value | cat > ./screenshot.png
