a15e1c581cb4789fd1c6d6b0d6886b1c112e5d04
ivan <byteofmydream@gmail.com>
2018-02-24 02:01:32 +0200

    screenshoter

---
+++ screenshot.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -x
+sessionId=`cat ./sessionId`
+url="http://127.0.0.1:4444/wd/hub/session/$sessionId/screenshot"
+curl -s ${url} -H 'Pragma: no-cache' -H 'Origin: http://127.0.0.1:4444' -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json; charset=utf-8'
