5a6c4536cf1eed05dc463dda7d73ed0e78c31b26
Iseedead <sivkovych.p@gmail.com>
2018-03-11 10:27:25 +0200

    typo

--- close.sh
+++ close.sh
@@ -2,4 +2,4 @@
 sessionId=`cat ./sessionId`
 url="${BASE_URL}:${HUB_PORT}/${RELATIVE_URL}/session/$sessionId"
 response=`curl -s -X "DELETE" ${url} -H "Pragma: no-cache" -H "Origin: ${BASE_URL}:${HUB_PORT}"`
-./log.sh ${response} "Browser has been successfully closed" "Failed to close browser"
+./log.sh ${response} "Browser was successfully closed" "Failed to close browser"
