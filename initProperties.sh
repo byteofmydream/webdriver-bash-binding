a49af75e76a3ae2ab0693ca04f20f097174bd6a1
Iseedead <sivkovych.p@gmail.com>
2018-03-13 23:23:59 +0200

    Support of empty lines and comment in .properties file

--- initProperties.sh
+++ initProperties.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
+
 while IFS="=" read -r propName propValue || [ -n "$propName" ];
 do
-    eval FOO='$'${propName}
-    if [ -z ${FOO} ]
+    if [[ -n "${propName}" ]]
     then
-        export "$propName=$propValue"
-    else
-        echo "Env. variable ${propName} is busy, choose another name"
+        if [[ ${propName} != "#"* ]]
+        then
+            eval FOO='$'${propName}
+            if [ -z ${FOO} ]
+            then
+                export "$propName=$propValue"
+            else
+                echo "Env. variable ${propName} is busy, choose another name"
+        #        exit 1
+            fi
+        fi
     fi
 done < config.properties
+exit 0
--- obtainJSResult.sh
+++ obtainJSResult.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 sessionId=`cat ./sessionId`
 url="${BASE_URL}:${HUB_PORT}/${RELATIVE_URL}/session/$sessionId/execute"
 result=`curl -s "$url" -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json; charset=utf-8' --data '{"script":"return window[\"WebDriverScriptResult\"];","args":[""]}'`
