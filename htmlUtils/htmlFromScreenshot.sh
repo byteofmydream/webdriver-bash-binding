c2e79a54b135f07ec90c81b2f475410b31a1b9e8
ivan <byteofmydream@gmail.com>
2018-03-22 00:43:22 +0200

    first step for file project structure refactoring

--- htmlUtils/htmlFromScreenshot.sh
+++ htmlUtils/htmlFromScreenshot.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-set -x
 prefix="<img src=\"data:image/png;base64,"
-base64=`cat | ./getFromJSON.sh value | cat`
+base64=`cat | ../getFromJSON.sh value | cat`
 postfix="\"/>"
 echo "$prefix$base64$postfix"
